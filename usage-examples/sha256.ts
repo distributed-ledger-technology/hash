955419ad5984ed405444c6d439617fc8d4ca8ef2
monique <michael.spengler@fh-hwz.ch>
2023-12-27 12:57:37 +0100

    update and support Freedom Cash

--- usage-examples/sha256.ts
+++ usage-examples/sha256.ts
@@ -1,4 +1,3 @@
-// for details check https://www.npmjs.com/package/hash.js
 import hashJs from 'https://deno.land/x/hash/mod-hashjs.ts'
 
 console.log(hashJs.sha256().update('Bitcoin').digest('hex'))
