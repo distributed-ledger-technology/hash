2139c1e63b6198857df970f1c644f640a4962352
monique <michael.spengler@fh-hwz.ch>
2023-12-27 10:02:51 +0100

    update due to much simpler npm ... usage now

--- mod-crc32.ts
+++ mod-crc32.ts
@@ -1,3 +1,3 @@
 
-import crc32 from "https://cdn.esm.sh/v57/crc-32@1.2.0/es2021/crc-32.js"
+import crc32 from "npm:hash.js"
 export default crc32
--- mod-hashjs.ts
+++ mod-hashjs.ts
@@ -1,3 +1,3 @@
 
-import hashjs from "https://cdn.esm.sh/v57/hash.js@1.1.7/es2021/hash.js";
+import hashjs from "npm:hash.js";
 export default hashjs
